db827f2560e402f26d6a5cd212b10d0ec50ea9ba
m14775611609 <m14775611609@163.com>
2024-06-09 13:57:11 +0800

    [需求][用户输入错误继续输入chapter6.7][yangxinxin] 当用户输入非数字时通过clear和cin.get()读取到\n，继续输入新的数字

--- ConsoleApplicationcprimer/ConsoleApplicationcprimer.cpp
+++ ConsoleApplicationcprimer/ConsoleApplicationcprimer.cpp
@@ -3301,7 +3301,7 @@ int main()
 	// cin.get();
 	return 0;
 }
-*/
+
 // jump.cpp -- using continue and break
 #include <iostream>
 const int ArSize = 80;
@@ -3326,6 +3326,80 @@ int main()
 	}
 	cout << "\n" << spaces << " spaces\n";
 	cout << "Done.\n";
+	// cin.get();
+	// cin.get();
+	return 0;
+}
+
+
+// cinfish.cpp -- non-numeric input terminates loop
+#include <iostream>
+const int Max = 5;
+int main()
+{
+	using namespace std;
+	// get data
+	double fish[Max];
+	cout << "Please enter the weights of your fish.\n";
+	cout << "You may enter up to " << Max
+		<< " fish <q to terminate>.\n";
+	cout << "fish #1: ";
+	int i = 0;
+	while (i < Max && cin >> fish[i]) {
+		if (++i < Max)
+			cout << "fish #" << i + 1 << ": ";
+	}
+	// calculate average
+	double total = 0.0;
+	for (int j = 0; j < i; j++)
+		total += fish[j];
+	// report results
+	if (i == 0)
+		cout << "No fish\n";
+	else
+		cout << total / i << " = average weight of "
+		<< i << " fish\n";
+	cout << "Done.\n";
+	// code to keep VC execution window open if q is entered
+	//	if (!cin)  // input terminated by non-numeric response
+	//	{
+	//	    cin.clear();  // reset input
+	//	    cin.get();    // read q
+	//	}
+	//	cin.get();    // read end of line after last input
+	//	cin.get();    // wait for user to press <Enter>
+	return 0;
+}
+*/
+// cingolf.cpp -- non-numeric input skipped
+#include <iostream>
+const int Max = 5;
+int main()
+{
+	using namespace std;
+	// get data
+	int golf[Max];
+	cout << "Please enter your golf scores.\n";
+	cout << "You must enter " << Max << " rounds.\n";
+	int i;
+	for (i = 0; i < Max; i++)
+	{
+		cout << "round #" << i + 1 << ": ";
+		while (!(cin >> golf[i]))
+		{
+			cin.clear();
+			while (cin.get() != '\n')
+				continue;
+			cout << "Please enter number:";
+		}
+	}
+	// calculate average
+	double total = 0.0;
+	for (i = 0; i < Max; i++)
+		total += golf[i];
+	// report results
+	cout << total / Max << " = average score "
+		<< Max << " rounds\n";
 	// cin.get();
 	// cin.get();
 	return 0;
